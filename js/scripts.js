727001e95f60fa18cd2f3a13986e40c005a325fd
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2024-01-03 00:00:14 -0300

    continuando mechando nos slides

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 var swiper = new Swiper(".slide", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
