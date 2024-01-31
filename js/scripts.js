fb9418a63f2056b1b4b14011ff115f4f92f6968f
Gabriel Lourenço de Oliveira <126710780+Gabriel-Oliviera42@users.noreply.github.com>
2024-01-31 15:58:52 -0300

    muidei o swiper para seu uma galeria

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,4 @@
-var swiper = new Swiper(".slide", {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+  });
